beb7e01026c1727781cf44bfcc5fe37caac5a132
Akhil Raj <akhilraaaj7@gmail.com>
2023-12-22 14:21:58 +0530

    commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/jsx-no-target-blank */
 import DragonBallImg from '../assets/footerImg.png';
+import DragonBallImg1 from '../assets/dragonimg.png';
 
 const Footer = () => {
   return (
@@ -12,7 +13,7 @@ const Footer = () => {
         <a href='/'><img src={DragonBallImg} alt="Dragon Ball Image" width={200} height={180} className="px-10 py-4" /></a>
       </div>
       <hr className="border-t border-gray-300 border-dotted w-120 items-center" />
-      <span className="block font-bold text-[#FFE4D6] text-center p-4 text-xl"><a href="https://github.com/akhilraaaj" target="_blank">&lt;&gt; by <span className='underline'>Akhil Raj</span></a></span>
+      <span className="flex items-center justify-center font-bold text-[#FFE4D6]  p-4 text-xl"><a href="https://github.com/akhilraaaj" target="_blank"><img src={DragonBallImg1} width={150} height={150} /></a></span>
     </div>
   );
 };
