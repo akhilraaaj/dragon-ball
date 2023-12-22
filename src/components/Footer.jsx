7633c562dd1a4ace8a8977a11632f94c3d8021b4
Akhil Raj <akhilraaaj7@gmail.com>
2023-12-22 14:26:02 +0530

    commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
           <h1 className="text-xl font-extrabold text-[#26577C] mb-4">Characters: <span className='text-[#FFCC70]'>58</span></h1>
           <h1 className="text-xl font-extrabold text-[#26577C]">Transformations: <span className='text-[#FFCC70]'>49</span></h1>
         </div>
-        <a href='/'><img src={DragonBallImg} alt="Dragon Ball Image" width={200} height={180} className="px-10 py-4" /></a>
+        <a href='/'><img src={DragonBallImg} alt="Dragon Ball Image" width={200} height={180} className="px-10 py-4" /></a> 
       </div>
       <hr className="border-t border-gray-300 border-dotted w-120 items-center" />
       <span className="flex items-center justify-center font-bold text-[#FFE4D6]  p-4 text-xl"><a href="https://github.com/akhilraaaj" target="_blank"><img src={DragonBallImg1} width={150} height={150} /></a></span>
